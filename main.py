0c1a98fe0d7e57199123fcdac7eb95b53ff00fb3
jaycode <teguhwpurwanto@gmail.com>
2022-02-28 18:59:16 +0700

    fix: Checks graded versions too

--- main.py
+++ main.py
@@ -8,21 +8,21 @@ import pandas as pd
 import os
 import random
 
-def add_to_metrics(metrics, feedback, is_meet, data_row=None):
+def add_to_metrics(metrics, feedback, is_passed, data_row=None):
     """ Update metrics and optionally data row with given feedback.
 
     Args:
         metrics(dict): The metrics dict.
         feedback(list): The feedback list from Udacity's student_feedbacks page
         (responses > feedback)
-        is_meet(Boolean): True if the project meets specifications.
+        is_passed(Boolean): True if the project meets specifications.
         data_row(pd.Series): A row from the data.csv file.
     """
     for f in feedback:
-        if is_meet:
-            metrics[f['key']]['meets'].append(int(f['rating']))
+        if is_passed:
+            metrics[f['key']]['passed'].append(int(f['rating']))
         else:
-            metrics[f['key']]['fails'].append(int(f['rating']))
+            metrics[f['key']]['failed'].append(int(f['rating']))
         if data_row is not None:
             data_row[f['key']] = int(f['rating'])
 
@@ -31,90 +31,94 @@ def calc_metrics(metrics, max_rating=5):
     """
     results = {}
     for k in metrics:
-        total_meets = len(metrics[k]['meets']) * max_rating
-        total_fails = len(metrics[k]['fails']) * max_rating
-        avg_meets = sum(metrics[k]['meets']) / total_meets
-        avg_fails = sum(metrics[k]['fails']) / total_fails
-        avg = (sum(metrics[k]['meets']) + sum(metrics[k]['fails'])) \
+        total_meets = len(metrics[k]['passed']) * max_rating
+        total_fails = len(metrics[k]['failed']) * max_rating
+        avg_meets = sum(metrics[k]['passed']) / total_meets
+        avg_fails = sum(metrics[k]['failed']) / total_fails
+        avg = (sum(metrics[k]['passed']) + sum(metrics[k]['failed'])) \
               / (total_meets + total_fails)
-        results[k] = "{:.2f} ({:.2f} meets {:.2f} fails)".format(avg, avg_meets, avg_fails)
+        results[k] = "{:.2f} ({:.2f} passed {:.2f} failed)".format(avg, avg_meets, avg_fails)
     return results
 
-def calc(input_file, data_file, udacity_browser=None, max_num_meets=10, max_num_fails=10):
+def calc(input_file, data_file, udacity_browser=None, max_num_passed=10, max_num_failed=10):
     """ Calculate scores.
 
     Args:
         input_file(str): Path to input file.
         data_file(str): Path to the data file (csv).
         udacity_browser(UdacityBrowser): A UdacityBrowser object.
-        max_num_meets(int): The number of "meet specifications" reviews to calculate.
-        max_num_fails(int): The number of "required changes" reviews to calculate.
+        max_num_passed(int): The number of "meet specifications" reviews to calculate.
+        max_num_failed(int): The number of "required changes" reviews to calculate.
     """
     # shortcut to udacity_connection parameter.
     b = udacity_browser
 
     metrics = {
-        'review_use': {'meets': [], 'fails': []},
-        'review_clarity': {'meets': [], 'fails': []},
-        'review_detail': {'meets': [], 'fails': []},
-        'review_personal': {'meets': [], 'fails': []},
-        'review_unbiased': {'meets': [], 'fails': []}
+        'review_use': {'passed': [], 'failed': []},
+        'review_clarity': {'passed': [], 'failed': []},
+        'review_detail': {'passed': [], 'failed': []},
+        'review_personal': {'passed': [], 'failed': []},
+        'review_unbiased': {'passed': [], 'failed': []}
     }
 
     # Loading work in progress file if exists.
     if os.path.exists(data_file):
         data_df = pd.read_csv(data_file)
     else:
-        data_df = pd.DataFrame(columns=['submission_id', 'is_included', 'is_meet'] + list(metrics.keys()))
+        data_df = pd.DataFrame(columns=['submission_id', 'is_passed', 'graded_version'] + list(metrics.keys()))
 
+    data_df.graded_version = data_df.graded_version.astype(str)
     with open(input_file) as f:
         data = json.load(f)
 
-        num_meets = 0
-        num_fails = 0
+        num_passed = 0
+        num_failed = 0
         i = 0
-        while num_meets < max_num_meets or num_fails < max_num_fails:
+        while num_passed < max_num_passed or num_failed < max_num_failed:
             row = data[i]
             if b is not None and row['submission_id'] not in data_df['submission_id'].values:
                 data_row = {k: None for k in metrics.keys()}
                 data_row['submission_id'] = row['submission_id']
-                data_row['is_included'] = False
-                is_meet = b.is_meet_specifications(row['submission_id'], wait=random.uniform(1, 20))
-                if is_meet:
-                    data_row['is_meet'] = True
-                    if num_meets < max_num_meets:
-                        print("Status: Meet specifications ({} found)".format(num_meets+1))
-                        add_to_metrics(metrics, row['responses']['feedback'], is_meet, data_row=data_row)
-                        data_row['is_included'] = True
-                    num_meets += 1
-                else:
-                    data_row['is_meet'] = False
-                    if num_fails < max_num_fails:
-                        print("Status: Requires changes ({} found)".format(num_fails+1))
-                        add_to_metrics(metrics, row['responses']['feedback'], is_meet, data_row=data_row)
-                        data_row['is_included'] = True
-                    num_fails += 1
-                data_df = data_df.append(data_row, ignore_index=True)
+                is_passed = b.get_is_passed(row['submission_id'], wait=random.uniform(0, 1))
+                graded_version = b.get_graded_version(max_graded_version=2, wait=random.uniform(4, 15))
+                data_row['graded_version'] = graded_version
+                if graded_version <= 2:
+                    if is_passed:
+                        data_row['is_passed'] = True
+                        if num_passed < max_num_passed:
+                            print("Status: Meet specifications ({} found)".format(num_passed+1))
+                            add_to_metrics(metrics, row['responses']['feedback'], is_passed, data_row=data_row)
+                        num_passed += 1
+                    else:
+                        data_row['is_passed'] = False
+                        if num_failed < max_num_failed:
+                            print("Status: Requires changes ({} found)".format(num_failed+1))
+                            add_to_metrics(metrics, row['responses']['feedback'], is_passed, data_row=data_row)
+                        num_failed += 1
+                    data_df = data_df.append(data_row, ignore_index=True)
                 data_df.to_csv(data_file, index=False)
             else:
                 print("Work-in-progress submission ID #{}".format(row['submission_id']))
                 data_row = data_df[data_df['submission_id'] == row['submission_id']].iloc[0]
-                is_meet = data_row['is_meet']
-                is_included = data_row['is_included']
-                feedback = []
-                if is_included:
-                    for k in metrics:
-                        value = data_row[k]
-                        feedback.append({'key': k, 'rating': value})
-                    add_to_metrics(metrics, feedback, is_meet)
-                if is_meet == 1:
-                    if num_meets < max_num_meets:
-                        print("Status: Meet specifications ({} found)".format(num_meets+1))
-                    num_meets += 1
-                else:
-                    if num_fails < max_num_fails:
-                        print("Status: Requires changes ({} found)".format(num_fails+1))
-                    num_fails += 1
+                is_passed = data_row['is_passed']
+                graded_version = data_row['graded_version']
+                if graded_version.isnumeric() and int(graded_version) <= 2:
+                    feedback = []
+                    def include_me():
+                        for k in metrics:
+                            value = data_row[k]
+                            feedback.append({'key': k, 'rating': value})
+                        add_to_metrics(metrics, feedback, is_passed)
+                    if is_passed == 1:
+                        if num_passed < max_num_passed:
+                            include_me()
+                            print("Status: Meet specifications ({} found)".format(num_passed+1))
+                        num_passed += 1
+                    else:
+                        if num_failed < max_num_failed:
+                            include_me()
+                            print("Status: Requires changes ({} found)".format(num_failed+1))
+                        num_failed += 1
             i += 1
     print("Metrics:")
     pprint(metrics)
--- modules/udacity_browser.py
+++ modules/udacity_browser.py
@@ -1,5 +1,6 @@
 from .browser import Browser
 import time
+from selenium.common.exceptions import TimeoutException
 
 class UdacityBrowser(Browser):
     REVIEW_URL_T = "https://review.udacity.com/#!/reviews/{}"
@@ -24,9 +25,9 @@ class UdacityBrowser(Browser):
         print("Wait for {} seconds...".format(wait))
         return self.driver.find_element_by_css_selector('body > pre').text
 
-    def is_meet_specifications(self, submission_id, wait=1):
+    def get_is_passed(self, submission_id, wait=1):
         url = self.REVIEW_URL_T.format(submission_id)
-        print("Checking review page {}".format(url))
+        print("Checking if this review had passed: {}".format(url))
         self.driver.get(url)
         css_classes = ["result-label", "h-slim-top", "ng-binding"]
         status = self._find_els_by_classes(tag="h2", classes=css_classes, operator="and")[0]
@@ -35,4 +36,84 @@ class UdacityBrowser(Browser):
         if "Requires Changes" in status.text:
             return False
         else:
+            return True
+
+    def get_graded_version(self, submission_id=None, max_graded_version=2, wait=1):
+        """ Get project's graded version.
+
+        Rejected projects are not taken into account.
+        """
+        if submission_id is not None:
+            url = self.REVIEW_URL_T.format(submission_id)
+            print("Checking the grade of this review: {}".format(url))
+            self.driver.get(url)
+
+        try:
+            # Find the History tab (a href with text "History" whose ancestor
+            # does not have a class "ng-hide").
+            history = self._find_el_by_xpath(
+                "//a[contains(normalize-space(text()),'History')" + \
+                " and not(ancestor::*[contains(@class, 'ng-hide')])]")
+            print("History found")
+            history.click()
+
+            # Find review links whose ancestor does not have a class "ng-hide"/
+            review_links = self._find_els_by_xpath(
+                "//a[contains(@class, 'review-list-name')" + \
+                " and not(ancestor::*[contains(@class, 'ng-hide')])]"
+            )
+
+        except TimeoutException:
+            print("History not found")
+            return 1
+
+        # Find this review's version (not ungraded version) i.e. h2 with class
+        # 'current-review-name' whose ancestor does not have a class "ng-hide",
+        # then get its first span.
+        this_review = self._find_el_by_xpath(
+            "//h2[contains(@class, 'current-review-name')" + \
+            " and not(ancestor::*[contains(@class, 'ng-hide')])]/span[1]"
+        )
+        version = int(this_review.text.split("#")[1])
+
+        other_versions = [int(l.text.split("#")[1]) for l in review_links]
+        other_versions.reverse()
+        other_ids = [l.get_attribute('href').split("/")[-1] for l in review_links]
+        other_ids.reverse()
+        other_submissions = dict(zip(other_versions, other_ids))
+        print("Found submission Ids:")
+        print(other_submissions)
+        graded_version = 1
+        for k in other_submissions:
+            if k < version:
+                if self.is_graded(other_submissions[k]):
+                    graded_version += 1
+                    if graded_version > max_graded_version:
+                        graded_version = ">{}".format(max_graded_version)
+                        break
+
+        print("Graded version: {}".format(graded_version))
+        print("Wait for {} seconds...".format(wait))
+        time.sleep(wait)
+        return graded_version
+
+    def is_graded(self, submission_id, wait=1):
+        url = self.REVIEW_URL_T.format(submission_id)
+        print("Checking if this review was graded: {}".format(url))
+        self.driver.get(url)
+
+        try:
+            # Get the un-hidden "Unable to review" element:
+            ungraded_element = self._find_el_by_xpath(
+                "//h2[contains(normalize-space(text()),'Unable to review')" +
+                " and not(ancestor::*[contains(@class, 'ng-hide')])]"
+            )
+            print("Review was ungraded")
+            print("Wait for {} seconds...".format(wait))
+            time.sleep(wait)
+            return False
+        except TimeoutException:
+            print("Review was graded")
+            print("Wait for {} seconds...".format(wait))
+            time.sleep(wait)
             return True
